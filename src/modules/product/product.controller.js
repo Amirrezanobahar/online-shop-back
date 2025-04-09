0309d93a6920423f27d558125b5e3d31f0081ce3
Amirrezanobahar <nwbharamyrrda@gmail.com>
2025-04-09 19:18:48 +0330

    product.controller.js updated

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -4,13 +4,13 @@ import Product from './../../models/product.js';
 export const createProduct = async (req, res) => {
   try {
     const product = await Product.create(req.body);
+    // todo validation 
     res.status(201).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-// دریافت محصولات با فیلتر
 export const getProduct = async (req, res, next) => {
   try {
 
@@ -20,6 +20,21 @@ export const getProduct = async (req, res, next) => {
 
     if (!product) {
       return res.status(404).send({ error: ' product not found' })
+    }
+    res.send(product);
+
+  } catch (error) {
+    next(error)
+  }
+};
+
+export const allProducts = async (req, res, next) => {
+  try {
+
+    const product = await Product.find({}).lean()
+
+    if (!product) {
+      return res.status(404).send({ error: 'not exist any product' })
     }
     res.send(product);
 
--- src/modules/product/product.router.js
+++ src/modules/product/product.router.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { auth } from "./../../middlewares/auth.js";
-import {createProduct,getProduct} from './product.controller.js'
+import {createProduct,getProduct,allProducts} from './product.controller.js'
 
 
 const router = express.Router()
 
+
+router.get('/',allProducts)
 router.get('/:id',getProduct)
 router.post('/',createProduct)
 
