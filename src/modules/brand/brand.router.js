4df2f7888d70cf0d0c92fdb0419f1f33dd7af555
Amirrezanobahar <nwbharamyrrda@gmail.com>
2025-04-11 20:52:31 +0330

    add delete api for brand

--- src/modules/brand/brand.router.js
+++ src/modules/brand/brand.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getAllBrands,getBrand,createBrand } from './brand.controller.js'
+import { deleteBrand,getAllBrands,getBrand,createBrand } from './brand.controller.js'
 
 const router =new express.Router()
 
 router.get('/',getAllBrands)
 router.get('/:id',getBrand)
 router.post('/',createBrand)
+router.post('/:id',deleteBrand)
 
 export default router;
